3bfe9dfd9f0031692fe4e449411e26f928c8cc8a
chadgalusha <chadgalusha83@gmail.com>
2022-10-05 17:29:02 -0500

    updated Expense/Income History model. added range data annotation to decimal properties

--- MyBudget/Models/ExpenseHistory.cs
+++ MyBudget/Models/ExpenseHistory.cs
@@ -8,10 +8,17 @@ namespace MyBudget.Models
         [PrimaryKey, AutoIncrement]
         public int ExpenseHistoryId { get; set; }
 
-        public int ExpenseId { get; set; }
+        [MaxLength(50)]
+        public string ExpenseName { get; set; }
 
+        [System.ComponentModel.DataAnnotations.Range(0, 999999999.99)]
         public decimal ExpenseAmount { get; set; }
 
+        [System.ComponentModel.DataAnnotations.Range(0, 999999999.99)]
+        public decimal AmountPaid { get; set; }
+
         public DateTime ExpenseDate { get; set; }
+
+        public DateTime DueDate { get; set; }
     }
 }
--- MyBudget/Models/Expenses.cs
+++ MyBudget/Models/Expenses.cs
@@ -15,6 +15,7 @@ namespace MyBudget.Models
 
         public int PaymentFrequencyTypeId { get; set; }
 
+        [System.ComponentModel.DataAnnotations.Range(0, 999999999.99)]
         public decimal ExpenseAmount { get; set; }
 
         public DateTime InitialExpenseDate { get; set; }
